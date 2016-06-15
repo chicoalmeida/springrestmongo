bbd0cd21cdca43f787ab1cc44f3488881a5404b4
Chico <francisco.almeida@yaman.com.br>
2016-06-15 16:54:52 -0300

    add authetication support

--- src/main/java/br/com/chico/sample/configuration/SecurityConfig.java
+++ src/main/java/br/com/chico/sample/configuration/SecurityConfig.java
@@ -1,7 +1,10 @@
 package br.com.chico.sample.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,4 +16,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * This method ensures that all requests are secured and that basic authentication is used.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated();
+        http.httpBasic();
+        http.csrf().disable();
+    }
+
+    /**
+     * Valid users and password are kept in memory and are configured in this method.
+     * In a real-world application you would of course use something else here like database, LDAP or OAuth.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("admin").password("pwd").roles("ADMIN", "USER");
+    }
 }
